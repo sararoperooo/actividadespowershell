8c2e91af4322c51cccbb01fd8d6b4b04ba44692e
Sara Ropero <sarropmor@alu.edu.gva.es>
2026-01-12 08:58:09 +0100

    subo el resto de ejercicios de la práctica 1 y los primeros de la práctica 2

---
+++ ejercicio1condicionales.ps1
@@ -0,0 +1,7 @@
+﻿$num = Read-Host "Introduce un número: "
+If ($num%2 -eq 0){
+    Write-Host "El número $num es par"
+}
+Else{
+    Write-Host "El número $num es impar"
+}
---
+++ ejercicio2condicionales.ps1
@@ -0,0 +1,7 @@
+﻿$edad = Read-Host "Introduce una edad: "
+If ($edad -lt 18){
+    Write-Host "Es menor de edad"
+}
+Else{
+    Write-Host "Es mayor de edad"
+}
