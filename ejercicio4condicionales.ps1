1d9b0aaa6c17df823dca242e78a9940d4f544464
Sara Ropero <sarropmor@alu.edu.gva.es>
2026-01-12 09:32:05 +0100

    Sbo hasta el ejercicio 5 de la práctica 2

---
+++ ejercicio4condicionales.ps1
@@ -0,0 +1,32 @@
+﻿[int] $num1 = Read-Host "Introduce un número: "
+[int] $num2 = Read-Host "Introduce otro número: "
+Write-Host "****** CALCULADORA ******"
+Write-Host "1.Sumar"
+Write-Host "2.Restar"
+Write-Host "3.Multiplicar"
+Write-Host "4.Dividir"
+$opcion = Read-Host "¿Qué desea hacer? Elige una opción"
+switch ($opcion) {
+        {$_ -eq "Sumar"}{
+            $resultado = $num1 + $num2
+            Write-Host "El resultado de la operación es: $resultado"
+            }
+        
+        {$_ -eq "Restar"}{
+            $resultado = $num1 - $num2
+            Write-Host "El resultado de la operación es: $resultado"
+            }
+        
+        {$_ -eq "Multiplicar"}{
+            $resultado = $num1 * $num2
+            Write-Host "El resultado de la operación es: $resultado"
+            }
+        {$_ -eq "Dividir"}{
+            $resultado = $num1 / $num2
+            Write-Host "El resultado de la operación es: $resultado"
+            }
+        {default}{
+            Write-Host "Introduce una opción válida"
+        }
+    
+        
---
+++ ejercicio5condicionales.ps1
@@ -0,0 +1,7 @@
+﻿$fichero = Read-Host "Intorduce un fichero o carpeta: "
+If (Test-Path $fichero){
+    Write-Host "El elemento existe"
+    }
+Else{
+     Write-Host "El elemento no existe"
+    }
