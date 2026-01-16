1169f8f52ae6046f40dc1118d63065936ea5bec7
Sara Ropero <sarropmor@alu.edu.gva.es>
2026-01-16 13:21:28 +0100

    Subo del 6 al 8

---
+++ ejercicio6condicionales.ps1
@@ -0,0 +1,8 @@
+﻿$elemento = Read-Host "Introduce un directorio: "
+If (Test-Path $elemento -PathType Container) {
+  Write-Host "La ruta indicada es un directorio: "
+  Get-ChildItem -Path $elemento -Recurse
+  }
+  Else {
+    Write-Host "La ruta inidicada no es un directorio o no existe"
+    }
---
+++ ejercicio7condicionales.ps1
@@ -0,0 +1,8 @@
+﻿$contrasenya="hola01"
+$contrasenya_usu=Read-Host "Introduce una contraseña"
+If ($contrasenya -ieq $contrasenya_usu){
+    Write-Host "La contraseña introducida es correcta"
+    }
+    Else {
+    Write-Host "La contraseña introoducida es incorrecta"
+    }
