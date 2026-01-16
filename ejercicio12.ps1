718e2ee0e71dc4b18640ee408e3310856fdb3c57
Sara Ropero <sarropmor@alu.edu.gva.es>
2026-01-16 13:49:45 +0100

    Subo todos los ejercicios de condicionales

---
+++ ejercicio12.ps1
@@ -0,0 +1,8 @@
+﻿$tipo_pizza=Read-Host "Introduce que tipo de pizza quieres"
+If ($tipo_pizza -eq "Vegetariana"){
+    Write-Host "Puedes añadir un ingrediente más entre los siguientes: Pimiento y Tofu"
+
+}
+Else{
+ Write-Host "Puedes añadir un ingrediente más entre los siguientes: Peperoni, Jamón y salmón"   
+}
