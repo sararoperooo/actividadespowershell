0e74d6fef1945e2856776bd59b6eb8d16010f637
Sara Ropero <sarropmor@alu.edu.gva.es>
2026-01-19 09:26:43 +0100

    Ejercicios bucles

---
+++ ejercicio7bucles.ps1
@@ -0,0 +1,7 @@
+﻿$altura=Read-host "Introduce un número: "
+for ($i=1; $i -le $altura; $i++){
+       for($j=1; $j -le $i; $j++){
+        Write-Host "*"
+    }
+    Write-Host ""
+}
---
+++ ejercicio8bucles.ps1
@@ -0,0 +1,7 @@
+﻿$numero=Read-Host "Introduce un número"
+for ($i=1; $i -le $numero; $i+=2){
+    Write-Host $i
+    for ($j=1; $j -le $i; $j+=2){
+    Write-Host $j
+    }
+}
