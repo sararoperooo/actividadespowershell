cb8a787f0aef3d39e800af5bbfb995e4fdf3d54e
Sara Ropero <sarropmor@alu.edu.gva.es>
2026-01-23 13:42:18 +0100

    Subo el ejercicio 2 de funciones

---
+++ ejercicio2funciones.ps1
@@ -0,0 +1,5 @@
+﻿$usuarios = Import-Csv C:\Users\Administrador\Desktop\usuarios.csv -Delimiter ","
+
+foreach($usu in $usuarios){
+    Write-Host "Usuario: $($usu.nombre) $($usu.apellidos) $($usu.grupo)"
+}
